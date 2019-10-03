77d4a7f54d8d97735b79a86733d3e454f1d4b0f3
Phyo Khine <pkhine.2020@berkeley.edu>
2019-10-03 09:32:42 -0700

    Initial Commit

---
+++ tiptop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tiptop
+//
+//  Created by Elizabeth Chen on 10/3/19.
+//  Copyright © 2019 elizvictory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
